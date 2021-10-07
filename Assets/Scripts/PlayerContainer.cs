79f14b818f35d428cf10f82103e4441a8c9d1c8d
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-10-07 15:26:59 -0400

    Added Restore dll

--- Assets/Scripts/PlayerContainer.cs
+++ Assets/Scripts/PlayerContainer.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerContainer : MonoBehaviour
 {
     public int health = 10;
+    public Text text;
+
+    private void Update()
+    {
+        text.text = health.ToString();
+    }
 }
--- Assets/Scripts/PluginTester.cs
+++ Assets/Scripts/PluginTester.cs
@@ -8,19 +8,74 @@ public class PluginTester : MonoBehaviour
     public int damage = -2;
     public int heal = 1;
     int health;
+    bool isRestore = false;
 
     [DllImport("GameEnginesMidterm")]
     private static extern int AlterHealth(int h, int v);
 
+    [DllImport("GameEnginesMidterm")]
+    private static extern int DamageHealth(int h, int v);
+
+    [DllImport("GameEnginesMidterm")]
+    private static extern int HealHealth(int h, int v);
+
+    [DllImport("RestoreDLL")]
+    private static extern int AlterHealth();
+
+    [DllImport("RestoreDLL")]
+    private static extern int DamageHealth();
+
+    [DllImport("RestoreDLL")]
+    private static extern int HealHealth();
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            player.GetComponent<PlayerContainer>().health = AlterHealth(player.GetComponent<PlayerContainer>().health, damage);
+            isRestore = false;
         }
-        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            player.GetComponent<PlayerContainer>().health = AlterHealth(player.GetComponent<PlayerContainer>().health, heal);
+            isRestore = true;
+        }
+
+        if (!isRestore)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                player.GetComponent<PlayerContainer>().health = AlterHealth(player.GetComponent<PlayerContainer>().health, damage);
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                player.GetComponent<PlayerContainer>().health = AlterHealth(player.GetComponent<PlayerContainer>().health, heal);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                player.GetComponent<PlayerContainer>().health = DamageHealth(player.GetComponent<PlayerContainer>().health, damage);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                player.GetComponent<PlayerContainer>().health = HealHealth(player.GetComponent<PlayerContainer>().health, heal);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                player.GetComponent<PlayerContainer>().health = AlterHealth();
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                player.GetComponent<PlayerContainer>().health = AlterHealth();
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                player.GetComponent<PlayerContainer>().health = DamageHealth();
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                player.GetComponent<PlayerContainer>().health = HealHealth();
+            }
         }
     }
 }
